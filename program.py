b926234a13c7354a50d64407d25dd704662aba0e
Adrian Stein <afestein@gmail.com>
2022-09-26 17:36:09 +1000

    Load data from CSV

--- program.py
+++ program.py
@@ -1,1 +1,13 @@
-print("Hello, world")
+import numpy as np
+
+# Load data from CSV file
+def load_csv():
+    with open("data.csv") as file:
+        ncols = len(file.readline().split(","))
+        data = np.loadtxt(file, delimiter=",", usecols=range(1, ncols))
+    return data
+
+
+distances = load_csv()
+
+print(distances)
