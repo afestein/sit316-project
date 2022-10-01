da23cdd310ab3edf97b30b22dabd1f5e531154dc
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-10-01 20:21:10 +1000

    quick fix

--- utils.py
+++ utils.py
@@ -1,68 +1,7 @@
 import numpy as np
-from sklearn.decomposition import PCA
 from sklearn.manifold import MDS
 import matplotlib.pyplot as plt
 import pandas as pd
-import sympy
-
-def give_coords(distances):
-	"""give coordinates of points for which distances given
-
-	coordinates are given relatively. 1st point on origin, 2nd on x-axis, 3rd 
-	x-y plane and so on. Maximum n-1 dimentions for which n is the number
-	of points
-
-	 Args:
-		distanes (list): is a n x n, 2d array where distances[i][j] gives the distance 
-			from i to j assumed distances[i][j] == distances[j][i]
-
-	 Returns:
-		numpy.ndarray: cordinates in list form n dim
-
-	 Examples:
-		>>> a = sympy.sqrt(2)
-		>>> distances = [[0,1,1,1,1,1],
-						 [1,0,a,a,a,a],
-						 [1,a,0,a,a,a],
-						 [1,a,a,0,a,a],
-						 [1,a,a,a,0,a],
-						 [1,a,a,a,a,0]]
-		>>> give_coords(distances)
-		array([[0, 0, 0, 0, 0],
-			   [1, 0, 0, 0, 0],
-			   [0, 1, 0, 0, 0],
-			   [0, 0, 1, 0, 0],
-			   [0, 0, 0, 1, 0],
-			   [0, 0, 0, 0, 1]], dtype=object)
-
-		>>> give_coords([[0, 3, 4], [3, 0, 5], [4, 5, 0]])
-		array([[0, 0],
-		[3, 0],
-		[0, 4]], dtype=object)        
-
-	"""
-	distances = np.array(distances)
-
-	n = len(distances)
-	X = sympy.symarray('x', (n, n - 1))
-
-	for row in range(n):
-		X[row, row:] = [0] * (n - 1 - row)
-
-	for point2 in range(1, n):
-
-		expressions = []
-
-		for point1 in range(point2):
-			expression = np.sum((X[point1] - X[point2]) ** 2) 
-			expression -= distances[point1,point2] ** 2
-			expressions.append(expression)
-
-		X[point2,:point2] = sympy.solve(expressions, list(X[point2,:point2]))[1]
-
-	return X
-
-
 
 def load_csv(filename):
 	with open(filename) as file:
@@ -74,8 +13,8 @@ def load_csv(filename):
 def get2d(distance_matrix):
 	columns = [f"{i}" for i in range(len(distance_matrix[0]))]
 	df = pd.DataFrame(data=distance_matrix, columns=columns)
-	pca = MDS(n_components=2, dissimilarity="precomputed")
-	X2d = pca.fit_transform(df)
+	mds = MDS(n_components=2, dissimilarity="precomputed")
+	X2d = mds.fit_transform(df)
 	return X2d
 
 
