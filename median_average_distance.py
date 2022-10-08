efbdf3e920ffb095fdd55326a030bb75a50c72c3
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-10-08 13:14:45 +1100

    documented median algorithm

--- median_average_distance.py
+++ median_average_distance.py
@@ -3,21 +3,25 @@ from utils import get_cost, load_csv, plot_solution, print_output, get_median, c
 import time
 from neighbourhood_search import run_neighbourhood_search
 
+
+# A naive algorithm which uses statistical analysis to obtain solutions
 def median_avg(cities, num_stations):
+	# obtain a list of the average distance to all other cities for each city
 	averages = {}
 	for i, city in enumerate(cities):
 		city_avg = np.average(city)
 		averages[i] = city_avg
 
-
+	# sort the list of average distances
 	sorted_avgs = {key: val for key, val in sorted(
 		averages.items(), key=lambda ele: ele[1], reverse=False)}
 
 	medians = []
+	# fill a quarter of the solution with the lowest average distanced cities
 	for key, val in list(sorted_avgs.items())[:num_stations//4]:
 		medians.append(key)
 		del sorted_avgs[key]
-
+	# fill the rest of the solution with the cities with the median distance to all other cities
 	while len(medians) < num_stations:
 		median = get_median(sorted_avgs)
 		medians.append(median)
@@ -25,22 +29,17 @@ def median_avg(cities, num_stations):
 		del sorted_avgs[median]
 		
 	sol = [1 if x in medians else 0 for x in averages.keys()]
-
 	best_cost = get_cost(sol, cities)
 
 	return sol, best_cost
 
 
 if __name__ == "__main__":
-
 	cities = load_csv("./data/500_cities.csv")
-
 	num_stations = 100
 
 	start = time.time()
-
 	solution, best_cost = median_avg(cities, num_stations)
-
 	end = time.time() - start
 
 	print_output("Median Average", end, best_cost, len(cities), num_stations)
